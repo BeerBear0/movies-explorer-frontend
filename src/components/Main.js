36fab3d8e76897115a3fb9705d15fc730e5c1cee
Nikita Chernov <ns4ern@gmail.com>
2021-07-31 14:31:43 +0300

    add routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,3 +5,19 @@
 //     Techs — компонент с использованными технологиями.
 //     AboutMe — компонент с информацией о студенте.
 //     Portfolio — компонент со ссылками на другие проекты.
+
+import React from 'react';
+import AboutMe from './Main/AboutMe';
+import AboutProject from './Main/AboutProject';
+import NavTab from './Main/NavTab';
+import Portfolio from './Main/Portfolio';
+import Promo from './Main/Promo';
+import Techs from './Main/Techs';
+
+function Main (props) {
+    return (
+        <>
+        </>
+    )
+}
+export default Main;
