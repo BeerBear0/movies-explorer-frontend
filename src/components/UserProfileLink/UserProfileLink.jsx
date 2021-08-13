78dfcaae5c1e64aa6033099a76f30518b6ad9109
Nikita Chernov <ns4ern@gmail.com>
2021-08-13 20:29:14 +0300

    delete

--- src/components/UserProfileLink/UserProfileLink.jsx
+++
@@ -1,9 +0,0 @@
-import { NavLink } from 'react-router-dom';
-import './UserProfileLink.css'
-
-
-export const UserProfileLink = () => {
-  return (
-    <NavLink className="user-profile-link" to="/profile">Аккаунт</NavLink>
-  )
-}
