a3cb7400ffef0fcf55881ad145eb4b9d13717f7f
Nikita Chernov <ns4ern@gmail.com>
2021-08-17 04:23:55 +0300

    hotfix card bugs

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,10 +1,3 @@
-import Movies from "../components/Movies/Movies";
-
-export const headerHref1 = '/signup';
-// export const headerHref1 = 'https://movies.nikko.nomoredomains.monster/signup';
-export const headerHref2 = '/signin';
-// export const headerHref1 = 'https://movies.nikko.nomoredomains.monster/signin';
-
 
 export const movies = [
     {
