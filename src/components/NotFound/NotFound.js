759124ae51b9e22507b7aac0e0ba6cb8eedec04d
Nikita Chernov <ns4ern@gmail.com>
2021-09-06 00:47:03 +0300

    hotfix btnback

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
+import {NavLink} from "react-router-dom";
+
 function NotFound () {
     return (
         <div className='notfound'>
             <h1 className='notfound__error'>404</h1>
             <p className='notfound__message'>Страница не найдена</p>
-            <button className='notfound__btn'>Назад</button>
+            <NavLink to='/' className='notfound__btn'>Назад</NavLink>
         </div>
     )
 }
