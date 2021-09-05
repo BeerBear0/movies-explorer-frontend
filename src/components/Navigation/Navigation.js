4db31df248c72906f471e29fd71c4eb73c19be91
Nikita Chernov <ns4ern@gmail.com>
2021-09-06 00:36:44 +0300

    hotfix

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,13 +8,13 @@ function Navigation(props) {
     const location = useLocation();
 
     return (
-        <nav className={`navigation ${props.loggedIn ? 'navigation__display' : ''}`}>
+        <nav className={`navigation ${loggedIn ? 'navigation__display' : ''}`}>
             <NavLink to={headerHref1} className={`navigation__link ${location.pathname === '/' ? '' : 'navigation__link-style navigation__link_loggedin'}`}>{linkNum1}</NavLink>
             <div className={`navigation__link-div ${location.pathname === '/' ? '' : 'navigation__link-div_loggedin '} ${loggedIn ? 'navigation__link__div_logged' : ''}`}>
                 <NavLink to={headerHref2} className={`navigation__link navigation__link_loggedin ${location.pathname === '/' ? '' : 'navigation__link-style_two'}`}>{linkNum2}</NavLink>
             </div>
-            <NavLink to='/profile' className={`navigation__account ${location.pathname === '/' ? 'navigation__account_display' : ''}`}>Аккаунт</NavLink>
-            <button type='button' className={`navigation__btn ${location.pathname === '/' ? 'navigation__account_display' : ''}`} />
+            <NavLink to='/profile' className={`navigation__account ${loggedIn ? '' : 'navigation__account_display'}${location.pathname === '/' ? 'color__white' : 'color__black'}`}>Аккаунт</NavLink>
+            <button type='button' className={`navigation__btn ${loggedIn  ? '' : 'navigation__account_display'}`} />
         </nav>
     )
 }
